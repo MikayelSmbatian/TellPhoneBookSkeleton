005b82094ecf64b65cb9d8f87af167bf8427e048
Mikayel <mikayel1998@gmail.com>
2016-09-02 01:45:44 +0400

    SPB

--- src/ControlerPack/Controller.java
+++ src/ControlerPack/Controller.java
@@ -6,13 +6,11 @@ interface CreatingControllerI
     {
         return null;
     }
-    public static boolean creatUserDB()
+    public static String creatingHELP()
     {
-        //should create a DB folder for User.
-        //Return true if folder was created successfully
-
-        return false;
+        return null;
     }
+
 }
 interface EditingControllerI
 {
@@ -23,7 +21,7 @@ interface EditingControllerI
         //Otherwise throws exception or returns null
         return null;
     }
-    public static boolean addFriend(String username)
+    public static boolean addFriend(User us,String usernameFriend)
     {
         //gets username User from DB
         //checks friendship
@@ -32,7 +30,7 @@ interface EditingControllerI
         //returns true if everything is ok
         return true;
     }
-    public static boolean deleteFriend(String username)
+    public static boolean deleteFriend(User us,String usernameFriend)
     {
         //gets "User with username" from DB.
         //throws exception if such user is not found in DB
@@ -40,6 +38,27 @@ interface EditingControllerI
         //returns false if such user weren't in friendship with Signed-In user.
         //returns true if everything is ok.
         return true;
+    }
+    public static void showTelNumbers(User user)
+    {
+        //checks if user exists in DB
+        //shows tel numbers
+    }
+    public static void showFriends(User user)
+    {
+        //checks if user exists in DB
+        //shows friends
+    }
+    public static boolean addTelNumber(User user, String telNumber)
+    {
+        //returns false if telNumber is already in List
+        //graceful exception if user not found
+        //returns true if already is ok
+    }
+    public static String editingHELP()
+    {
+        //
+        return null;
     }
     public static void signOut()
     {
